34409ae8dedd2ca126e4897d7be71cdacf72e974
Laurent Gauthier <laurent.gauthier@soccasys.com>
2017-04-13 00:07:00 +0200

    Update code with license/copyright

--- marker-design/mkPattern.py
+++ marker-design/mkPattern.py
@@ -1,9 +1,26 @@
+#  
+# Copyright (C) 2016-2017 Laurent GAUTHIER <laurent.gauthier@soccasys.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+#
+
 import sys
 import rs
 import struct
 
-# 07.01.2015 Slightly increase the size of the black dots as it is what seems to limit the range of detection.
-# 11.01.2015 Started implementation of Reed-Solomon protected code.
+# 07.01.2016 Slightly increase the size of the black dots as it is what
+#            seems to limit the range of detection.
+# 11.01.2016 Started implementation of Reed-Solomon protected code.
 
 class PatternGenerator:
     def __init__(self, size):
--- tracking-demo/src/marker.h
+++ tracking-demo/src/marker.h
@@ -1,11 +1,19 @@
-/*
- * marker.h
- *
- *  Created on: Jan 5, 2016
- *      Author: Laurent Gauthier <mr.l.gauthier@gmail.com>
- *
+/*  
  * Find markers in a picture, and for each marker verify and decode an ID.
  *
+ * Copyright (C) 2016-2017 Laurent GAUTHIER <laurent.gauthier@soccasys.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
  */
 
 #ifndef SRC_MARKER_H_
